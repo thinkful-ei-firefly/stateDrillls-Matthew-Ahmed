c64c44b5b8fb35da7cc96f005beedcbcb5c462c0
Ahmed <ahmed.max7586@gmail.com>
2019-08-18 09:21:03 -0400

    creat Accordion component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 import "./index.css";
 import RouletteGun from "./state-drills/RouletteGun";
 // import Bomb from "./state-drills/Bomb";
 // import HelloWorld from "./state-drills/HelloWorld";
 
-ReactDOM.render(<RouletteGun />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
